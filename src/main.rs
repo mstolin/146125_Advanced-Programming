d4b09d59a8bcf25b5d6cf72848a310d63ffc49dd
Farid0U <94851163+Farid0U@users.noreply.github.com>
2023-01-12 21:05:42 +0100

    Goal of main
    Just comments on how to divide the code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
 }
+
+//main code can go here and each implementation of visualizers (something that creates a pdf or a page) 
+//and strategies (simply doing some interaction with markets) can go on our own folders
+//this way everyone can manage his own code as preferred and we use the common part to present all the work
