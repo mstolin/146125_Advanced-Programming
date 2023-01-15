7a3a3f068c475b83e2b93b882ca9add08fb7f924
Farid0U <ouedraogofarid@gmail.com>
2023-01-15 16:29:04 +0100

    Added plot visualizer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+mod fo_visualzer;
+
 fn main() {
-    println!("Hello, world!");
+    fo_visualzer::render_plot();
 }
 
-//main code can go here and each implementation of visualizers (something that creates a pdf or a page) 
+//main code can go here and each implementation of visualizers (something that creates a pdf or a page)
 //and strategies (simply doing some interaction with markets) can go on our own folders
 //this way everyone can manage his own code as preferred and we use the common part to present all the work
