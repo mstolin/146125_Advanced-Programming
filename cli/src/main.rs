05fb699fdfb01e356c1b36bddfc4d4e84d20540b
Marcel Stolin <kuri-dv@mailbox.org>
2023-01-15 22:26:30 +0100

    Updated dependencies

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,9 +5,9 @@ use std::rc::Rc;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 use unitn_market_2022::market::Market;
-/*use SGX::market::sgx::SGX;
+use SGX::market::sgx::SGX;
 use TASE::TASE;
-use ZSE::market::ZSE;*/
+use ZSE::market::ZSE;
 
 type MarketRef = Rc<RefCell<dyn Market>>;
 
@@ -37,14 +37,12 @@ fn main() {
     // parse arguments
     //let strategy =
 
-    let s =
-
     // Init markets
-    /*let mut sgx = SGX::new_random();
+    let mut sgx = SGX::new_random();
     let mut smse = Smse::new_random();
     let mut tase = TASE::new_random();
     let mut zse = ZSE::new_random();
-    let markets = Vec::from([sgx, smse, tase, zse]);*/
+    let markets = Vec::from([sgx, smse, tase, zse]);
     let markets = Vec::new();
 
     // Definition for main loop
