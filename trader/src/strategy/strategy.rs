93596c9b51d513272c25d08b38ac66a587a20a53
Marcel Stolin <kuri-dv@mailbox.org>
2023-01-16 20:30:23 +0100

    Formatted source code

--- trader/src/strategy/strategy.rs
+++ trader/src/strategy/strategy.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 use unitn_market_2022::good::good::Good;
 
 pub trait Strategy {
-    fn new() -> Self where Self: Sized;
+    fn new() -> Self
+    where
+        Self: Sized;
     fn apply(&mut self, markets: &mut Vec<MarketRef>, goods: &mut Vec<Good>);
 }
