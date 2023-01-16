704279bd8e31f0e6a752bbba50538321fa00a74b
Marcel Stolin <kuri-dv@mailbox.org>
2023-01-16 20:29:13 +0100

    Remove get_results method and make instance Sized

--- trader/src/strategy/strategy.rs
+++ trader/src/strategy/strategy.rs
@@ -2,10 +2,7 @@ use crate::MarketRef;
 use std::collections::HashMap;
 use unitn_market_2022::good::good::Good;
 
-pub type StrategyResult = HashMap<String, i32>;
-
 pub trait Strategy {
-    fn new() -> Self;
+    fn new() -> Self where Self: Sized;
     fn apply(&mut self, markets: &mut Vec<MarketRef>, goods: &mut Vec<Good>);
-    fn get_result(&self) -> StrategyResult;
 }
