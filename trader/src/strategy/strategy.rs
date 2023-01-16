5825f43b0c5e380acac26b931236ccd0365a27ea
Marcel Stolin <kuri-dv@mailbox.org>
2023-01-16 16:16:00 +0100

    Updated strategy trait

--- trader/src/strategy/strategy.rs
+++ trader/src/strategy/strategy.rs
@@ -1,9 +1,11 @@
 use crate::MarketRef;
 use std::collections::HashMap;
+use unitn_market_2022::good::good::Good;
 
 pub type StrategyResult = HashMap<String, i32>;
 
 pub trait Strategy {
-    fn apply(&mut self, markets: &Vec<MarketRef>);
+    fn new() -> Self;
+    fn apply(&mut self, markets: &mut Vec<MarketRef>, goods: &mut Vec<Good>);
     fn get_result(&self) -> StrategyResult;
 }
