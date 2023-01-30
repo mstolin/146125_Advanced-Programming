7d47de639af60efce0c4303e90b78ace27378455
Marcel Stolin <kuri-dv@mailbox.org>
2023-01-30 20:54:52 +0100

    Formatted code

--- trader/src/lib.rs
+++ trader/src/lib.rs
@@ -2,9 +2,9 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use unitn_market_2022::market::Market;
 
+mod consts;
 pub mod strategy;
 mod tests;
 pub mod trader;
-mod consts;
 
 type MarketRef = Rc<RefCell<dyn Market>>;
